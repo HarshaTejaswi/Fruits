5433e284aca242837214142db0a9ade099632cbc
Harsha Tejaswi <harshatejaswi4@gmail.com>
2024-06-16 20:15:40 +0530

    Initial

---
+++ backend/app/models.py
@@ -0,0 +1,11 @@
+from .database import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    hashed_password = db.Column(db.String(120), nullable=False)
+
+class Fruit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(120))
