a727873b8db33d70c81d105dfb1da0da50b7c69d
Harsha Tejaswi <harshatejaswi4@gmail.com>
2024-06-18 17:59:15 +0530

    Update app.py

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,15 +1,8 @@
-# app.py
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'your_database_uri_here'
 db = SQLAlchemy(app)
 
-# Import routes or blueprints
-from app.routes import *
-
-if __name__ == "__main__":
-    app.run()
+# Your models definitions go here
